2c7028c923d1809b594c796e22d575f08f70ada2
Eric Ranschau <eranschau@mac.com>
2020-03-09 21:43:16 -0500

    - Headline styles.

--- index.php
+++ index.php
@@ -19,24 +19,88 @@ get_header();
 
 <main id="site-content" role="main">
     <section class="container">
-        <h1>Growing Leaders</h1>
-        <h2>Growing Leaders</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="row">
+            <div class="col-md-10 offset-md-1">
+                <h1 class="heading-size-2 text-center">Growing Leaders.<br /> Developing Organizations.<br />Achieving Results.</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <h2 class="heading-size-3">True Partners in Evolving Organizations and Developing Leaders</h2>
+                <p>For additional support in reaching your organizational goals, Lily Pad Consulting is the dedicated partner for your business.</p>
+                <p>Partnering with our brand carries the promise of unique learning experiences that drive thought-provoking visioning, impactful strategies and leader development while having fun and achieving results.</p>
+            </div>
+        </div>
     </section>
+
     <section class="container">
-        <h1>Growing Leaders</h1>
-        <h2>Growing Leaders</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="row">
+            <div class="col-md-10 offset-md-2">
+                <h1 class="heading-size-1 withMargin">Seizing Opportunities</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 offset-md-4">
+                <h2 class="heading-size-4">Solutions That Make An Organizational Impact</h2>
+                <p>Through deep business and broad industry experiences, we partner with leaders to explore solutions that develop vision and strategy, drive alignment and implementation, develop talent and facilitate change.</p>
+                <p>Below you can learn more about the solutions we provide, the clients we support and the tools that give us results.</p>
+            </div>
+        </div>
     </section>
+
     <section class="container">
-        <h1>Growing Leaders</h1>
-        <h2>Growing Leaders</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="row hr">
+            <div class="col-md-9 offset-md-2">
+                <h5>Organization Development</h5>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-10 offset-md-2">
+                <h1 class="heading-size-1 withMargin">Building Capacity</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 offset-md-4">
+                <p>Effective leaders know the value of strengthening and expanding organizational capacity to affect change. Using a systemic and thoughtful approach, our goal is to increase your organization’s effectiveness – through people, processes and systems.</p>
+                <p>Areas Of Focus</p>
+                <ul>
+                    <li>Leader Development (Including 360° Assessments, Emotional Intelligence instruments, MBTI certification and Customized Development Programs) </li>
+                    <li>Team Effectiveness</li>
+                    <li>Change Strategies</li>
+                    <li>Executive Coaching</li>
+                    <li>Performance Management</li>
+                    <li>Talent Management</li>
+                    <li>Succession Planning</li>
+                    <li>Employee Engagement</li>
+                    <li>Cultural Revitalization</li>
+                </ul>
+            </div>
+        </div>
     </section>
+
     <section class="container">
-        <h1>Growing Leaders</h1>
-        <h2>Growing Leaders</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="row hr">
+            <div class="col-md-9 offset-md-2">
+                <h5>Board of Director Governance</h5>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-10 offset-md-2">
+                <h1 class="heading-size-1 withMargin">Equipping Your Board</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 offset-md-4">
+                <p>Your board is critical to the growth and success of your organization. Board governance involves equipping your board to strategically lead, with vision, passion, discernment and productive relationships.</p>
+                <p>Areas Of Focus</p>
+                <ul>
+                    <li>Board Development Assessments</li>
+                    <li>Comprehensive Board Engagement Plans</li>
+                    <li>Board &amp; Committee Structure and Alignment</li>
+                    <li>Board &amp; Committee Position Descriptions</li>
+                </ul>
+            </div>
+        </div>
     </section>
 </main><!-- #site-content -->
 
